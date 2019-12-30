0dd5cd6cbe45823d03dbeb70668d5665c5a4400e
Miles Rausch <miles@milesrausch.com>
2019-12-30 13:07:14 -0600

    Output to public folder instead of dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+const merge = require('webpack-merge');
 const path = require('path');
 const { createDefaultConfig } = require('@open-wc/building-webpack');
 
-module.exports = createDefaultConfig({
+const config = createDefaultConfig({
   input: path.resolve(__dirname, 'index.html'),
+});
+
+module.exports = merge(config, {
+  output: {
+    path: path.resolve(__dirname, 'public'),
+  },
 });
