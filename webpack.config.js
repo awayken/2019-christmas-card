0532b256e7a9934dc09f07e1033bfd47336515cb
Miles Rausch <miles@milesrausch.com>
2019-12-30 00:29:31 -0600

    Fix issue where index had moved

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,5 @@ const path = require('path');
 const { createDefaultConfig } = require('@open-wc/building-webpack');
 
 module.exports = createDefaultConfig({
-  input: path.resolve(__dirname, 'src', 'index.html'),
+  input: path.resolve(__dirname, 'index.html'),
 });
